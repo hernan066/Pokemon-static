6d27518d1c9dfe6923a300bb5487e656b5ae2d78
hernangit config --global user.name John <hernan0066@gmail.com>
2022-03-05 21:42:57 -0300

    Pokemon-page

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { SmallPokemon } from '../../interfaces/pokemon-list';
 import { Card, Grid, Row, Text } from "@nextui-org/react";
+import { useRouter } from 'next/router';
 
 
 interface Props{
@@ -8,10 +9,21 @@ interface Props{
 }
 
 export const PokemonCard: FC<Props> = ({pokemon}) => {
+    
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push(`/pokemon/${pokemon.id}`);
+    }
+    
     return(
         <Grid key={pokemon.id} xs={6} sm={3} md={2} xl={1}>
             
-        <Card hoverable clickable>
+        <Card 
+        hoverable 
+        clickable
+        onClick={handleClick}
+        >
           <Card.Body css={{p: 1}}>
             <Card.Image 
               src={pokemon.img}
